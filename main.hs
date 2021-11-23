cd05339cdb8c604e2723ac18be6995ef64f72523
Dreary Lisper <drearylisper@gmail.com>
2021-11-23 15:06:40 +0000

    Problem 02.

--- main.hs
+++ main.hs
@@ -6,5 +6,12 @@ p01 [x] = x
 p01 (_:xs) = p01 xs
 
 
+p02 :: [a] -> a
+p02 [] = error "List cannot be empty"
+p02 [_] = error "List is too small"
+p02 [x, _] = x
+p02 (_:xs) = p02 xs
+
+
 main :: IO()
 main = print "Hello, world"
